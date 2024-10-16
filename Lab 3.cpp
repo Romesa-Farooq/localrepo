c6cf405878cd42ac6879b22b12787a1c5e2d0a6c
Romesa-Farooq <romesafarooq5@gmail.com>
2024-10-16 11:12:26 +0500

    Add Initial files

---
+++ Lab 3.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int num1;
+   int num2;
+
+   cout<<"Enter First Value"<<endl;
+   cin>>num1;
+   cout<<"Enter Second Value"<<endl;
+   cin>>num2;
+
+   cout<<"By Applying Equal Operator "<<(num1 == num2 )<<endl;
+    cout<<"By Applying Not Equal Operator "<<(num1 != num2 )<<endl;
+    cout<<"By Applying Less than Operator "<<(num1 < num2 )<<endl;
+    cout<<"By Applying Greater than Operator "<<(num1 > num2 )<<endl;
+    cout<<"By Applying Less than equal to Operator "<<(num1 <= num2 )<<endl;
+    cout<<"By Applying Greater than equal to Operator "<<(num1 >= num2 )<<endl;
+   
+
+
+
+    return 0;
+    
+}
